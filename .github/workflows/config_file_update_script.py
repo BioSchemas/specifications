38f2d54bb55d0f8825d5f61be995554d07aca66a
Sahar Frikha <sahar.frikha1@gmail.com>
2022-10-19 11:28:04 +0200

    some final rectifications

--- .github/workflows/config_file_update_script.py
+++ .github/workflows/config_file_update_script.py
@@ -13,7 +13,8 @@ from yaml.loader import SafeLoader
 ## Main Script
 print(Fore.YELLOW + "Started updating profile versions config file" + Style.RESET_ALL)
 
-profile_verions_file = "./bioschemas.github.io/_data/profile_versions.yaml"
+profile_verions_file = "profile_versions.yaml"
+# profile_verions_file = "./bioschemas.github.io/_data/profile_versions.yaml"
 
 if path.exists(profile_verions_file):
     with open(profile_verions_file, "r", encoding="utf-8") as f:
