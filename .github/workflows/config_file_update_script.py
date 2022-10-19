5557a7893ec6ef8b415580c81690a5c514d4ec63
Sahar Frikha <sahar.frikha1@gmail.com>
2022-10-19 11:01:57 +0200

    some final rectifications

---
+++ .github/workflows/config_file_update_script.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+import yaml
+from colorama import Fore
+from colorama import Style
+import sys
+import csv
+from os import path
+from os import listdir
+from os.path import isfile, join
+
+
+## Main Script
+print(Fore.YELLOW + 'Started updating profile versions config file' + Style.RESET_ALL)
+
+profile_verions_file = "./bioschemas.github.io/_data/profile_versions.yaml"
+
+if path.exists(profile_verions_file):
+    print ("file esists")
+
+
+    
+    with open(profile_verions_file) as f:
+        data = yaml.load(f)
+        print(data)
+
+    
+    print(Style.BRIGHT + "Profile versions updated " + profile_verions_file + Style.RESET_ALL)                    
